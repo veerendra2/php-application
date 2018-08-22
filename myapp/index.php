0ae9a6be521bbf7e257655fc91a18bfddd1b2f7c
veerendra2 <veerendra2@github.com>
2018-08-22 16:58:38 +0530

    Rolling v2

--- myapp/index.php
+++ myapp/index.php
@@ -1,5 +1,5 @@
 <?php
-$app_version="1.0";
+$app_version="2.0";
 $database = "employees";
 $servername = gethostbyname(getenv("MYSQL_HOST"));
 $username = getenv("USERNAME");
@@ -10,9 +10,9 @@ $conn = mysqli_connect($servername, $username, $password, $database);
 if ($conn->connect_error) {
 	    die("Connection failed: " . $conn->connect_error);
 }
-$sql = "select first_name, last_name from employees where emp_no=".$random_emp_id;
+$sql = "select employees.first_name, employees.last_name, titles.title from employees,titles where employees.emp_no=".$random_emp_id." and titles.emp_no=".$random_emp_id;
 $result = mysqli_query($conn, $sql);
 $row=mysqli_fetch_assoc($result);
-echo("Random Employee Name:".$row["first_name"]." ".$row["last_name"]);
+echo("Random Employee Name:".$row["first_name"]." ".$row["last_name"]." Title".$row["title"]);
 echo("\nApp Version:".$app_version.". Responce from the POD:".$pod_name);
 ?>
